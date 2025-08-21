f7183acaeef7097581069fcde2cf8dac1820b675
rolandomunoz <rolando.muar@gmail.com>
2025-08-21 06:59:12 -0500

    updated

--- src/textgrid_manager/main.py
+++ src/textgrid_manager/main.py
@@ -7,7 +7,13 @@ from PySide6.QtWidgets import (
     QTableView,
     QVBoxLayout,
 )
-from PySide6.QtCore import QSortFilterProxyModel
+from PySide6.QtCore import (
+    QSortFilterProxyModel,
+)
+
+from PySide6.QtGui import (
+    QAction,
+)
 
 from textgrid_manager.models import TGTableModel
 
@@ -40,16 +46,40 @@ class TGManager(QMainWindow):
         self.setMinimumSize(800, 500)
         #self.showMaximized()
         self.init_ui()
+        self.init_actions()
         self.init_menubar()
+
+    def init_actions(self):
+        """
+        Create actions
+        """
+        #self.quit_act = QAction('&Salir', self)
+        #self.quit_act.triggered.connect(self.close)
+
+        self.filter_act = QAction('&Filter', self)
+        #self.filter_act.setIcon(QIcon(gear_icon_path.as_posix()))
+        #self.filter_act.triggered.connect(self.show_config)
+        self.filter_act.setShortcut('F')
 
     def init_menubar(self):
         menu_bar = QMenuBar()
-        main = menu_bar.addMenu('&Files')
+
+        files_bar = menu_bar.addMenu('&Files')
         self.setMenuBar(menu_bar)
+
+        edit_bar = menu_bar.addMenu('&Edit')
+        self.setMenuBar(menu_bar)
+
+        data_bar = menu_bar.addMenu('&Data')
+        self.setMenuBar(menu_bar)
+        data_bar.addAction(self.filter_act)
 
     def init_ui(self):
         self.editor_view = EditorView()
         self.setCentralWidget(self.editor_view)
+
+    def filter_table(self):
+        pass
 
 def run_app():
     app = QApplication([])
