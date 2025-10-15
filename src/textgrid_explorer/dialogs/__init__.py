e9e86b91e05a535b68301087a58649e36452fae3
rolandomunoz <rolando.muar@gmail.com>
2025-10-15 14:54:34 -0500

    updated

--- src/textgrid_explorer/dialogs/__init__.py
+++ src/textgrid_explorer/dialogs/__init__.py
@@ -1,5 +1,5 @@
-from textgrid_explorer.dialogs.init_wizard import InitWizard
 from textgrid_explorer.dialogs.filter_view import FilterView
+from textgrid_explorer.dialogs.filter_view import FilterByDialog
 from textgrid_explorer.dialogs.project_dlg import NewProjectDialog
 from textgrid_explorer.dialogs.project_dlg import OpenProjectDialog
 from textgrid_explorer.dialogs.project_dlg import ProjectSettingsDialog
--- src/textgrid_explorer/dialogs/filter_view.py
+++ src/textgrid_explorer/dialogs/filter_view.py
@@ -1,12 +1,67 @@
 from PySide6.QtWidgets import (
     QDialog,
     QLineEdit,
+    QComboBox,
     QPushButton,
     QFormLayout,
     QVBoxLayout,
     QHBoxLayout,
 )
 from PySide6.QtCore import Qt
+
+class FilterByDialog(QDialog):
+
+    def __init__(self, parent, fields=None, default_value=''):
+        super().__init__(parent)
+        self._fields = fields
+        self.default_value = default_value
+        if fields is None:
+            self._fields = []
+        self.setWindowTitle('Filter by')
+        self.init_ui()
+
+    def init_ui(self):
+        # Headers
+        self.headers_box = QComboBox(self)
+        self.headers_box.addItems(self._fields)
+
+        self.line_ed = QLineEdit(self.default_value, self)
+        self.line_ed.setPlaceholderText('Regex expression')
+
+        # Buttons
+        ok_btn = QPushButton('Ok', self)
+        ok_btn.clicked.connect(self.accept)
+
+        cancel_btn = QPushButton('Cancel', self)
+        cancel_btn.clicked.connect(self.reject)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(ok_btn)
+        hbox.addWidget(cancel_btn)
+
+        # Main Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.headers_box)
+        layout.addWidget(self.line_ed)
+        layout.addLayout(hbox)
+
+        self.setLayout(layout)
+
+    def fields(self):
+        return self._fields
+
+    def set_fields(self, fields):
+        self._fields = fields
+        self.headers_box.clear()
+        self.headers_box.addItems(self._fields)
+
+    def data(self):
+        field_index = -1
+        field_name = self.headers_box.currentText()
+        if field_name in self._fields:
+            field_index = self._fields.index(field_name)
+        field_value = self.line_ed.text()
+        return field_index, field_value
 
 class FilterView(QDialog):
 
