4e37f49881cef63c636b7551b5be12d6e5e085f7
rolandomunoz <rolando.muar@gmail.com>
2025-10-26 17:35:27 -0500

    feat: pick up the Praat name depending on the operating system when running for the first time

--- src/textgrid_explorer/__init__.py
+++ src/textgrid_explorer/__init__.py
@@ -1,11 +1,12 @@
+import platform
+
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QSettings
 
 from textgrid_explorer.explorer_window import TGExplorer
 
-settings = QSettings('Gilgamesh', 'TGExplorer')
-
 def init_preferences():
+    settings = QSettings('Gilgamesh', 'TGExplorer')
 
     if not settings.contains('data_dir'):
         settings.setValue('data_dir', '')
@@ -14,7 +15,14 @@ def init_preferences():
         settings.setValue('dict_path', '')
 
     if not settings.contains('praat/path'):
-        settings.setValue('praat/path', '')
+        praat_path = ''
+        if platform.system() == 'Windows':
+            praat_path = 'Praat.exe'
+        elif platform.system() == 'Linux':
+            praat_path = 'praat'
+        elif platform.system() == 'Darwin':
+            praat_path = 'praat'
+        settings.setValue('praat/path', praat_path)
 
     if not settings.contains('praat/maximize_audibility'):
         settings.setValue('praat/maximize_audibility', 0)
