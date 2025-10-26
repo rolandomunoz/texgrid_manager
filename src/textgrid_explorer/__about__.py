8ddb9cfcc2b2c5a3aa2305d653dfb62cf45c56ce
rolandomunoz <rolando.muar@gmail.com>
2025-10-26 18:22:59 -0500

    edit: add GNU GPL notes at the top lines

--- src/textgrid_explorer/__about__.py
+++ src/textgrid_explorer/__about__.py
@@ -1,4 +1,16 @@
-# SPDX-FileCopyrightText: 2025-present U.N. Owen <void@some.where>
+#!/usr/bin/env python
+#   textgrid_explorer - A TextGrid editing tool with a spreadsheet interface
+#   Copyright (C) 2025 Rolando Muñoz <rolando.muar@gmail.com>
 #
-# SPDX-License-Identifier: MIT
+#   This program is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License version 3, as published
+#   by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranties of
+#   MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program.  If not, see <https://www.gnu.org/licenses/>.
 __version__ = "0.0.1"
