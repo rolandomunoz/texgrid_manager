922f621e96131236024833d6b66a76bf365c7652
rolandomunoz <rolando.muar@gmail.com>
2025-10-26 18:25:18 -0500

    edit: add GNU GPL notes at the top lines

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
-#   textgrid_explorer - A TextGrid editing tool with a spreadsheet interface
-#   Copyright (C) 2025 Rolando Muñoz <rolando.muar@gmail.com>
-#
-#   This program is free software: you can redistribute it and/or modify it
-#   under the terms of the GNU General Public License version 3, as published
-#   by the Free Software Foundation.
-#
-#   This program is distributed in the hope that it will be useful, but
-#   WITHOUT ANY WARRANTY; without even the implied warranties of
-#   MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
-#   PURPOSE.  See the GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License along
+#!/usr/bin/env python
+#   textgrid_explorer - A TextGrid editing tool with a spreadsheet interface
+#   Copyright (C) 2025 Rolando Muñoz <rolando.muar@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License version 3, as published
+#   by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranties of
+#   MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
 #   with this program.  If not, see <https://www.gnu.org/licenses/>.
 import sys
 from pathlib import Path
