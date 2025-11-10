f23e07d90c745d8fb05a6edb1575232ffbf4d192
rolandomunoz <rolando.muar@gmail.com>
2025-11-10 01:28:05 -0500

    for compiling

---
+++ bin/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#   textgrid_explorer - A TextGrid editing tool with a spreadsheet interface
+#   Copyright (C) 2025 Rolando Muñoz <rolando.muar@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License version 3, as published
+#   by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranties of
+#   MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# nuitka-project: --mode=standalone
+# nuitka-project: --output-filename=textgrid_explorer_{OS}_{Arch}
+# nuitka-project: --enable-plugin=pyside6
+import sys
+from pathlib import Path
+
+package_dir = Path(__file__).parent.joinpath('..', 'src').resolve()
+sys.path.insert(0, str(package_dir))
+
+import textgrid_explorer
+
+if __name__ == '__main__':
+    textgrid_explorer.main()
